9852321858d965b26694395aab42cf69755b93ee
DHL-Sheffield <dhlee1@sheffield.ac.uk>
2025-01-15 17:27:22 +0000

    successful loop test run

--- loop.sh
+++ loop.sh
@@ -12,8 +12,8 @@ module load CFITSIO/3.48-GCCcore-9.3.0
 
 for beta in $(seq -1 0.1 1); do
   echo "Running for beta = $beta"
-  sed -i '/^\s*override:\s*$/ {n;s|^\s*beta_DE:\s*.*|      beta_DE: '"$beta"'|;}' beta.yaml
-  sed -i "s|^output:.*|output: ./beta/$beta|" beta.yaml
+  sed -i '/^\s*beta_DE:\s*$/ {n;s|^\s*value:\s*.*|    value: '"$beta"'|;}' beta.yaml
+  sed -i "s|^output:.*|output: ./beta/$beta-beta|" beta.yaml
   cat beta.yaml
   
   source /users/smp24dhl/cosmo/code/planck/clik/bin/clik_profile.sh
