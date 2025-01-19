aaed6a00bcfce109edcf4c3e84c79107f6e66737
Dong Ha's MBP <2dongha.lee@gmail.com>
2025-01-19 23:07:49 +0000

    test run on mac for looping

--- mac_loop.sh
+++ mac_loop.sh
@@ -1,10 +1,21 @@
 #!/bin/bash
 
-for beta in $(seq -0.1 0.001 0.1); do
-  echo "Running for beta = $beta"
-  sed -i '/^\s*beta_DE:\s*$/ {n;s|^\s*value:\s*.*|    value: '"$beta"'|;}' beta.yaml
-  sed -i "s|^output:.*|output: ./beta/$beta-beta|" beta.yaml
-  
-  source /users/smp24dhl/cosmo/code/planck/clik/bin/clik_profile.zsh
+for b in $(seq -0.1 0.01 0.1); do
+  echo "Running for beta = $b"
+
+  # Update the beta_DE value in the params file
+  sed -i '' -e '/^[[:space:]]*beta_DE:[[:space:]]*$/{
+    n
+    s|^[[:space:]]*value:[[:space:]]*.*|    value: '"$b"'|
+  }' beta.yaml
+
+  # Update the output directory
+  sed -i '' -e "s|^output:.*|output: ./beta/$b-beta|" beta.yaml
+
+  # print the updated yaml file
+  cat beta.yaml
+
+  # Run cobaya
+  source /Users/dongha/Projects/cosmo/code/planck/clik/bin/clik_profile.zsh
   mpirun -np 1 cobaya-run beta.yaml
 done
